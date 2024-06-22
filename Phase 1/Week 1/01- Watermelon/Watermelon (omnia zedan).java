ae1c83f6aa71a929fd37443289fb43ebd8c0cb60
omnia <oz1062003@gmil.com>
2024-06-23 01:25:00 +0200

    watermelon problem

---
+++ Phase 1/Week 1/01- Watermelon/Watermelon (omnia zedan).java
@@ -0,0 +1,19 @@
+//Author : omnia zedan
+
+import java.util.Scanner;
+
+
+public class Watermelon {
+
+    public static void main(String[] args) {
+    
+        Scanner w=new Scanner(System.in);
+        System.out.println("inset the weight of the watermelon");
+        int weight=w.nextInt();
+        if(weight%2==0)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+    
+}
