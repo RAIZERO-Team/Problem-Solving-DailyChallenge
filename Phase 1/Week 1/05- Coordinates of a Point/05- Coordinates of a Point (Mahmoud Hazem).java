e0cc44d17871bbd1c30a178295d64bede76f4b8d
Mahmoud <mahmoudhazem8910@gmail.com>
2024-06-26 06:08:43 +0300

    fifth problem

---
+++ Phase 1/Week 1/05- Coordinates of a Point/05- Coordinates of a Point (Mahmoud Hazem).java
@@ -0,0 +1,30 @@
+// Author: Mahmoud Hazem
+
+
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float x = input.nextFloat();
+        float y = input.nextFloat();
+        if(x > 0 && y > 0){
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        } else if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else if (x == 0 && y != 0) {
+            System.out.println("Eixo Y");
+        } else if (x != 0 && y == 0) {
+            System.out.println("Eixo X");
+        }
+    }
+}
