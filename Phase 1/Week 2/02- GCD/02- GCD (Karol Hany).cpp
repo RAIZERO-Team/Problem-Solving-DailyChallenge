d5b4d058cdc6a92b44389beefcd595f480ba22f7
karol <karolhany09@gmail.com>
2024-06-28 17:43:09 +0300

    the solution of second problem solving in week 2

---
+++ Phase 1/Week 2/02- GCD/02- GCD (Karol Hany).cpp
@@ -0,0 +1,18 @@
+// Author: Karol Hany
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+     //GCD
+
+    int num1, num2;
+    cin >> num1 >> num2;
+    if (num1 > num2)swap(num1, num2);
+    while (num1 > 0) {
+        num2 %= num1;
+        swap(num1, num2);
+    }
+    cout << num2;
+}
