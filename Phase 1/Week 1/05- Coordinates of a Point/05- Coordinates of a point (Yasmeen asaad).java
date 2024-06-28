1123fc8f12111df9a31b828092821366bf6ac84c
ahmed badawi <ahmedbad063@gmail.com>
2024-06-28 07:30:20 +0300

    solve the errors

--- Phase 1/Week 1/05- Coordinates of a Point/05- Coordinates of a point (Yasmeen asaad).java
+++ Phase 1/Week 1/05- Coordinates of a Point/05- Coordinates of a point (Yasmeen asaad).java
@@ -7,9 +7,6 @@ import java.util.Scanner;
  */
 public class Cordinate {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
       Scanner sc =new Scanner(System.in);
        float x=sc.nextFloat();
