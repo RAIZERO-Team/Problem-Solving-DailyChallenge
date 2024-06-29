55814c13129fe846f7c7cdd41eca7879e02e6502
yasmeen <yasmeenasaad429@gmail.com>
2024-06-30 00:51:33 +0300

    solve ps1 week2

---
+++ Phase 1/Week 2/01- Even, Odd, Positive and Negative/01- Even, Odd, Positive and Negative (Yasmeen asaad).java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+public class ps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc =new Scanner(System.in);
+        int N =sc.nextInt();
+         int even=0;
+         int odd=0;
+         int positive=0;
+         int neg=0;
+        for(int i=1;i<=N;i++){
+            int x=sc.nextInt();
+            if(x%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+            
+       if(x<0){
+           neg++;
+       }else if(x>0){
+           positive++;
+       }
+        }
+        System.out.println("Even: " +even);
+          System.out.println("Odd: " +odd);
+        System.out.println("Positive: " +positive);
+        System.out.println("Negative: " +neg);
+
+        
+    }
+    
+}
