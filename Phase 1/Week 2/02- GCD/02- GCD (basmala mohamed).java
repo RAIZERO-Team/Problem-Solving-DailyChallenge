50f0a0659d2187044130209d156f070fa6e2c1b9
basmala mohamed <basmallamx@gmail.com>
2024-07-01 01:34:10 +0300

    Solve second problem in the week2

---
+++ Phase 1/Week 2/02- GCD/02- GCD (basmala mohamed).java
@@ -0,0 +1,22 @@
+//Author: basmala mohamed
+
+import java.util.Scanner;
+
+
+public class GCD {
+
+    public static void main(String[] args) {
+        Scanner X = new Scanner(System.in);
+        
+        int A = X.nextInt();
+        int B = X.nextInt();
+        int result = 1;
+        
+        for (int i = 1; i <= Math.min(A, B); i++) {
+            if (A % i == 0 && B % i == 0) {
+                result = i;
+            }
+        }
+        System.out.println(result);
+    }
+}
