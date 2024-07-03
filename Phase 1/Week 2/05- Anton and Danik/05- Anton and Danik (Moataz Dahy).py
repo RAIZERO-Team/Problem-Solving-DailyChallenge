cacf0b01731663af2e5e27fd3cb396335928b0a4
Moataz <abdorashwan536@gmail.com>
2024-07-03 17:50:52 +0300

    Solve the fifth problem in Week 2

---
+++ Phase 1/Week 2/05- Anton and Danik/05- Anton and Danik (Moataz Dahy).py
@@ -0,0 +1,17 @@
+# Authot Moataz Dahy 
+
+N = int(input())
+A = str(input())
+
+print_A = A.count('A')
+print_D = A.count('D')
+print_F = "Friendship"
+
+if print_A > print_D:
+  print("Anton")
+
+elif print_D > print_A:
+  print("Danik")
+  
+else :
+  print(print_F)
