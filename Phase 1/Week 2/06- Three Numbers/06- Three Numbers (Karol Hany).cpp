fb79ca461398570b6304b41dc7015992530d6aba
karol <karolhany09@gmail.com>
2024-07-05 23:34:57 +0300

    problem solving num 6

---
+++ Phase 1/Week 2/06- Three Numbers/06- Three Numbers (Karol Hany).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int k, s, r = 0;
+    cin >> k >> s;
+    for (int i = 0; i <= k; i++) {
+        int z;
+        for (int j = 0; j <= k; j++) {
+            z = i + j;
+            if (s - z >= 0 && s - z <= k) {
+                r++;
+            }
+        }
+    }
+    cout << r << "\n";
+}
