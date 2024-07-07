6471a655bd7c6a2bc725d45e507f88298952efef
Mohamed <mohammedelkady8889@gmail.com>
2024-07-07 22:01:43 +0300

    week3_day2_1

---
+++ Phase 1/Week 3/02- Stones on the Table/02- Stones on the Table (mohamed khalifa).py
@@ -0,0 +1,11 @@
+# author : mohaemd khalifa
+n , colors = int(input()) , input()
+ch , counter= colors[0] , 0
+for i in  colors[1:] :
+    counter += 1 if i == ch else 0
+    ch = i 
+    
+print(counter)
+    
+
+    
