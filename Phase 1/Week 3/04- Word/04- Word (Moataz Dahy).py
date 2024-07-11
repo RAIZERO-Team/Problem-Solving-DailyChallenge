5ffb96dcf83931efb817ce3daca9e3617b2ee7a4
Moataz <abdorashwan536@gmail.com>
2024-07-11 21:27:11 +0300

    solve the Fourth problem in Week 3

---
+++ Phase 1/Week 3/04- Word/04- Word (Moataz Dahy).py
@@ -0,0 +1,17 @@
+#Author : Moataz Dahy
+
+s = input().strip()
+
+C_upper = 0 
+C_lower = 0 
+
+for i in s:
+    if i.islower():
+      C_lower += 1
+    elif i.isupper():
+      C_upper += 1
+
+if C_upper > C_lower:
+  print(s.upper())
+else :
+  print(s.lower())
