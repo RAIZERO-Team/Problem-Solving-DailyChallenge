2e2b3549e611a05ca2924a7df625140fe8e79346
Mohamed <mohammedelkady8889@gmail.com>
2024-07-12 02:05:13 +0300

    week3_day4

---
+++ Phase 1/Week 3/04- Word/04- Word (mohamed khalifa).py
@@ -0,0 +1,4 @@
+string = input()
+uppercase_letters = sum(1 for i in string if i.isupper())   
+lowercase_letters = sum(1 for i in string if i.islower())  
+print(string.upper() if uppercase_letters > lowercase_letters else string.lower())
