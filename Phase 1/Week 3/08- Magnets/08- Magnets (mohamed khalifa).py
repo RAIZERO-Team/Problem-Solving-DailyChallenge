41705c9817eb1816814562bd04302a849adcf98c
Mohamed <mohammedelkady8889@gmail.com>
2024-07-12 10:50:35 +0300

    last problem in week 3

---
+++ Phase 1/Week 3/08- Magnets/08- Magnets (mohamed khalifa).py
@@ -0,0 +1,11 @@
+n = int(input())
+num_of_groups = 1 
+magnets = input()
+divOrCon = magnets[1]
+for i in range(n-1) :
+    magnets = input()
+    num_of_groups += 1 if magnets[0] == divOrCon else 0 
+    divOrCon = magnets[1]      
+
+print(num_of_groups)   
+    
