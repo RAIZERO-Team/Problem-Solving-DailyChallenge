efd55e9ea8ea1d91dc23ff47191c4e593b2226d2
omnia <oz1062003@gmil.com>
2024-07-16 08:22:03 +0200

    Magnets

---
+++ Phase 1/Week 3/08- Magnets/08- Magnets (omnia zedan).java
@@ -0,0 +1,27 @@
+//Author:omnia zedan
+import java.util.Scanner;
+/**
+ *
+ * @author 3M.T
+ */
+public class  Magnets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        String arr[]=new String [n];
+       for(int i=0;i<arr.length;i++){
+           arr[i]=in.next();
+       } 
+       int fristnum;
+       int numofgroups=1;
+       for(int j=0;j<arr.length-1;j++){
+           if(!arr[j].equals(arr[j + 1]))
+           numofgroups++;
+       }
+       System.out.println(numofgroups);
+    }}
