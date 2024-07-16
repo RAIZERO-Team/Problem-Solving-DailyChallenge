925813067fbea0a2209bc42f0e45ef13bb8d4f7a
omnia <oz1062003@gmil.com>
2024-07-16 20:02:06 +0200

    Create A New String

---
+++ Phase 1/Week 4/01- Create A New String/01- Create A New String (Omnia Zedan).java
@@ -0,0 +1,17 @@
+//Author:omnia zedan
+import java.util.Scanner;
+/**
+ *
+ * @author 3M.T
+ */
+public class CreateANewString {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String word1=in.next();
+        String word2=in.next();
+       
+      System.out.println(word1.length()+" "+word2.length());
+      System.out.println(word1+" "+word2);
+    }}
