543b646cccb47144df101f391653423860c5607f
yasmeen <yasmeenasaad429@gmail.com>
2024-07-21 17:52:47 +0300

    solve new string promleb

---
+++ Phase 1/Week 4/01- Create A New String/01- Creat A New String (Yasmeen asaad).java
@@ -0,0 +1,23 @@
+//Author: Yasmeen Asaad
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yasmeen asaad
+ */
+public class Newstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in); 
+        String S=sc.next();
+        String T=sc.next();
+        System.out.println(S.length()+" "+T.length());
+        System.out.println(S+" "+T);
+    }
+    
+}
