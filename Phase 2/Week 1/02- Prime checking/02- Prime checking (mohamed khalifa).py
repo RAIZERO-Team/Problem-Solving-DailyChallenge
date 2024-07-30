12d630bafe48f8b9eaac9680217fcca3172fa74e
Mohamed <mohammedelkady8889@gmail.com>
2024-07-31 01:36:04 +0300

    solve problem 2

---
+++ Phase 2/Week 1/02- Prime checking/02- Prime checking (mohamed khalifa).py
@@ -0,0 +1,10 @@
+def prime(number) : 
+    if number in (0 , 1)  : return False  
+    if number == 2 : return True 
+    if number % 2 == 0 : return False  
+    for i in range(3, int(number**0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+print("YES"if  prime(int(input())) else "NO")
