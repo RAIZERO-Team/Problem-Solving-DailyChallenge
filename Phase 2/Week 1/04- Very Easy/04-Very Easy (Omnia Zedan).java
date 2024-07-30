e4b43ab01896fb9cbdfc20b7001290cab8410047
omnia <oz1062003@gmil.com>
2024-07-30 21:42:45 +0200

    Very Easy

---
+++ Phase 2/Week 1/04- Very Easy/04-Very Easy (Omnia Zedan).java
@@ -0,0 +1,18 @@
+//Author : Omnia zedan
+import java.util.Scanner;
+
+
+public class VeryEasy {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in =new Scanner (System.in);
+        long num1=in.nextInt();
+       long num2=in.nextInt();
+       long sum=num1+num2;
+       System.out.println(Math.abs(sum));
+       
+    }
+        
+       
+}
