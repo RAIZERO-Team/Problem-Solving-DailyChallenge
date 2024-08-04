d2fbcbc8bb0923c75b43708e3d3417e8ead16542
shahd <shahd.mahmoud1043@gmail.com>
2024-08-04 19:32:24 +0300

    Solve problem1

---
+++ Phase 2/Week 2/01- Easy task/01- Easy task (Shahd Mahmoud).java
@@ -0,0 +1,15 @@
+//Author: Shahd Mahmoud
+package easy.task;
+
+import java.util.Scanner;
+
+public class EasyTask {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long even = n / 2;
+        System.out.println(even);
+    }
+
+}
